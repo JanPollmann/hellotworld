b712c6f8d02c6dd1c5a715865ad96a04383a268c
Jan Pollmann <jan.f.pollmann@t-online.de>
2023-03-12 14:06:27 +0100

    initial commit

---
+++ src/HelloTworld.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+// example from http://derekmolloy.ie/hello-world-introductions-to-cmake/
+int main(int argc, char *argv[]){
+   std::cout << "Hello Tworld!" << std::endl;
+   return 0;
+}
