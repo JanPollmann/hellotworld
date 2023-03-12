b0d65af54924b8ec1329f32b9bece56ca960c35c
Jan Pollmann <jan.f.pollmann@t-online.de>
2023-03-12 21:28:00 +0100

    fixed clean

--- clean.sh
+++ clean.sh
@@ -3,8 +3,7 @@ echo "clean"
 rm -r CMakeFiles
 rm -r build
 rm -r _CPack_Packages
-rm hellotworld src/hellotworld
-rm libtworldstatic.a libtworldshared.so
+rm hellotworld src/hellotworld libtworld.*
 rm *.cmake CMakeCache.txt Makefile
 rm install_manifest.txt *.deb
 echo "clean done."
