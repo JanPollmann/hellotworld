3fc6da8f3b0744f70b6bbe282f0bb5a66771793a
Jan Pollmann <jan.f.pollmann@t-online.de>
2023-03-12 19:02:59 +0100

    clean build

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-echo "build (not clean!)"
+echo "clean"
+rm -r CMakeFiles
+rm HelloTworld
+rm src/HelloTworld
+rm CMakeCache.txt
+rm Makefile
+rm -r build
+rm cmake_install.cmake
 echo "cmake ..."
 cmake .
 echo "make ..."
