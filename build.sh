cee378583f9f5150bd7e0caf5a661e64286bbf94
Jan Pollmann <jan.f.pollmann@t-online.de>
2023-03-12 19:08:13 +0100

    gitignore

--- build.sh
+++ build.sh
@@ -7,8 +7,11 @@ rm CMakeCache.txt
 rm Makefile
 rm -r build
 rm cmake_install.cmake
+echo "create build dir"
+mkdir -p build/cmake
+cd build/cmake
 echo "cmake ..."
-cmake .
+cmake /../../.
 echo "make ..."
 make
 echo "done."
